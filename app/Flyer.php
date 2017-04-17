6efd345f5c4a02e52f5863aa1a94385f85fc8b89
Evgeniy <evgeniy.web@ukr.net>
2017-04-17 14:29:36 +0300

    relationsheeps & seeding done

--- app/Flyer.php
+++ app/Flyer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flyer extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
